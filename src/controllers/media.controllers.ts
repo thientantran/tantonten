ffee1785ad820e19d93dc6833977a6fa6c9680f5
thientantran <thientantm@gmail.com>
2024-05-16 23:30:32 +0700

    125. Upload a video

--- src/controllers/media.controllers.ts
+++ src/controllers/media.controllers.ts
@@ -15,7 +15,6 @@ export const uploadImage = async (req: Request, res: Response, next: NextFunctio
     maxFileSize: 3 * 1024 * 1024,
     filter: function ({ name, originalFilename, mimetype }) {
       const valid = name === 'image' && Boolean(mimetype?.startsWith('image/'))
-      console.log('originalFilename', originalFilename)
       if (!valid) {
         form.emit('error' as any, { status: 400, message: 'file is invalid' } as any)
       }
@@ -29,7 +28,6 @@ export const uploadImage = async (req: Request, res: Response, next: NextFunctio
   // tuy nhiên thực tế, filter sẽ lọc từng item trong files, nếu phần tử đầu tiên nó thoả nó vẫn nhảy vào parse như thường, mặc dùng files là rỗng
   // rất tào lao
   form.parse(req, async (error, fields, files) => {
-    console.log('fiels', files)
     if (error) {
       // return next({ status: 400, message: 'file too big' })
       if (error.code === 1009) {
@@ -80,4 +78,46 @@ export const uploadImage = async (req: Request, res: Response, next: NextFunctio
     })
   })
   // const data = await handleUploadOneImage(req, res)
+}
+
+export const uploadVideo = async (req: Request, res: Response, next: NextFunction) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('upload'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024 * 1024,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'video' && Boolean(mimetype?.startsWith('video/'))
+      console.log('mimetype', mimetype)
+      if (!valid) {
+        form.emit('error' as any, { status: 400, message: 'file is invalid' } as any)
+      }
+      return valid
+    }
+  })
+
+  form.parse(req, (error, fields, files) => {
+    if (error) {
+      if (error.code === 1009) {
+        // This is the error code for 'maxTotalFileSize exceeded'
+        return next({ status: 413, message: 'file too big' })
+      }
+      if (error.code === 1015) {
+        return next({ status: 400, message: 'Please upload 1 file' })
+      }
+      if (error.httpCode) {
+        error.status = error.httpCode
+      }
+      return next(error)
+    }
+    if (!files || !files.video) {
+      return next({ status: 400, message: 'file is required' })
+    }
+    const videoFile = files.video[0] // Access the first element of the File[] array
+    const videoName = path.basename(videoFile.filepath)
+    res.json({
+      file: !isProduction ? `${process.env.HOST}/upload/${videoName}` : `http://localhost:4000/upload/${videoName}`
+    })
+  })
 }
--- src/routes/media.route.ts
+++ src/routes/media.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { uploadImage } from '~/controllers/media.controllers'
+import { uploadImage, uploadVideo } from '~/controllers/media.controllers'
 
 const mediaRouter = Router()
 
 // mediaRouter.post('/upload-image', wrapSync(uploadImage))
 mediaRouter.post('/upload-image', uploadImage)
-
+mediaRouter.post('/upload-video', uploadVideo)
 export default mediaRouter

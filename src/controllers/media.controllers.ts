00ca97241fa79b925f63260f595fa1cbbfb85470
thientantran <thientantm@gmail.com>
2024-05-17 10:23:19 +0700

    126. CUstom video streaming

--- src/controllers/media.controllers.ts
+++ src/controllers/media.controllers.ts
@@ -117,7 +117,68 @@ export const uploadVideo = async (req: Request, res: Response, next: NextFunctio
     const videoFile = files.video[0] // Access the first element of the File[] array
     const videoName = path.basename(videoFile.filepath)
     res.json({
-      file: !isProduction ? `${process.env.HOST}/upload/${videoName}` : `http://localhost:4000/upload/${videoName}`
+      file: !isProduction
+        ? `${process.env.HOST}/media/video-stream/${videoName}`
+        : `http://localhost:4000/media/video-stream/${videoName}`
     })
   })
+}
+
+// export const serveVideoStreamController = (req: Request, res: Response, next: NextFunction) => {
+//   const range = req.headers.range
+//   if (!range) {
+//     console.log('range', range)
+//     return next({ status: 400, message: 'Range header is required' })
+//   }
+
+//   const { name } = req.params
+//   const videoPath = path.resolve('upload', name)
+//   const videoSize = fs.statSync(videoPath).size
+//   const chunkSize = 10 ** 6
+//   const start = Number(range.replace(/\D/g, ''))
+//   const end = Math.min(start + chunkSize, videoSize)
+//   const contentLength = 10 ** 6
+//   console.log(start - end)
+//   // const mime = (await import('mime')).default
+//   const contentType = 'video/mp4'
+//   const headers = {
+//     'Content-Range': `bytes ${0}-${end}/${videoSize}`,
+//     'Accept-Ranges': 'bytes',
+//     'Content-Length': contentLength,
+//     'Content-Type': contentType
+//   }
+//   console.log('headers', headers)
+//   res.writeHead(206, headers)
+//   const videoStream = fs.createReadStream(videoPath, { start, end })
+//   videoStream.pipe(res)
+// }
+export const serveVideoStreamController = async (req: Request, res: Response, next: NextFunction) => {
+  const range = req.headers.range
+  if (!range) {
+    return next({ status: 400, message: 'Range header is required' })
+  }
+
+  const { name } = req.params
+  const videoPath = path.resolve('upload', name)
+  const videoSize = fs.statSync(videoPath).size
+
+  const CHUNK_SIZE = 10 ** 6 // 1MB
+  const parts = range.replace(/bytes=/, '').split('-')
+  const start = parseInt(parts[0], 10)
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+
+  const contentLength = end - start + 1
+  const mime = (await import('mime')).default
+  const contentType = mime.getType(videoPath) || 'video/*'
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+
+  res.writeHead(206, headers)
+
+  const videoStream = fs.createReadStream(videoPath, { start, end })
+  videoStream.pipe(res)
 }
--- src/routes/media.route.ts
+++ src/routes/media.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { uploadImage, uploadVideo } from '~/controllers/media.controllers'
+import { serveVideoStreamController, uploadImage, uploadVideo } from '~/controllers/media.controllers'
 
 const mediaRouter = Router()
 
 // mediaRouter.post('/upload-image', wrapSync(uploadImage))
 mediaRouter.post('/upload-image', uploadImage)
 mediaRouter.post('/upload-video', uploadVideo)
+mediaRouter.get('/video-stream/:name', serveVideoStreamController)
 export default mediaRouter
